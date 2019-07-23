c3c65f8a836a7a19e646c9cd51125ee0ea126614
Rajvardhan99 <rajvardhan1999@gmail.com>
2019-07-23 18:40:09 +0530

    bug fixing

--- index.js
+++ index.js
@@ -55,8 +55,8 @@ app.post("/postroute", async (req, res, next) => {
     };
     await sgMail.send(msg);
     console.log(req.body);
-    // return res.json({ message: req.body });
-    return res.render("index", { title: "Express" });
+    return res.json({ message: req.body });
+    // return res.render("index", { title: "Express" });
   } catch (error) {
     return console.log(error);
   }
